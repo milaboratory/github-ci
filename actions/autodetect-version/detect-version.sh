32cb5156a3fafc45dd0746c3c20a53d79f4b5963
Korenevskiy Denis <denkoren@yandex.ru>
2021-11-18 16:07:43 +0300

    More debug messages

--- actions/autodetect-version/detect-version.sh
+++ actions/autodetect-version/detect-version.sh
@@ -12,23 +12,27 @@ source "${script_path}/../lib/ghwa-commands.sh"
 
 # Fetches all references
 function git_fetch_history() {
-    local _depth="${1}"
-    local _current_sha
+  local _depth="${1}"
+  local _current_sha
 
-    _current_sha="$(git rev-parse HEAD)"
+  ghwa_debug "Fetching repository git history with depth '${_depth}'..."
 
-    # Fetch all git tags first. We don't need their history,
-    # but we have to know their SHAs.
-    # This command breaks the LOCAL history of origin, overwriting
-    # it with single commit for each remote ref (grafted commit)
-    git fetch --depth=1 --tags origin
+  _current_sha="$(git rev-parse HEAD)"
 
-    # Fetch current commit's history up to ${_depth} records to restore
-    # HEAD's commits chain in hope we'll find at least one tag in there.
-    git fetch --depth="${_depth}" origin "${_current_sha}"
+  # Fetch all git tags first. We don't need their history,
+  # but we have to know their SHAs.
+  # This command breaks the LOCAL history of origin, overwriting
+  # it with single commit for each remote ref (grafted commit)
+  git fetch --depth=1 --tags origin
+
+  # Fetch current commit's history up to ${_depth} records to restore
+  # HEAD's commits chain in hope we'll find at least one tag in there.
+  git fetch --depth="${_depth}" origin "${_current_sha}"
 }
 
 function git_generate_version() {
+  ghwa_debug "Generating app version from git history and tags..."
+
   git describe --tags |
     awk '{sub("-", ".", $0); print $0}' | # v1.0-2-g<hash> -> v1.0.2-g<hash>
     sed -E 's/^v//;
@@ -39,11 +43,13 @@ function context_detect_version() {
   local _ref_type
   local _ref_name
 
+  ghwa_debug "Getting version number from current Action Context..."
+
   _ref_type=$(jq ".ref_type" <<<"${CONTEXT_GITHUB}")
   _ref_name=$(jq ".ref_name" <<<"${CONTEXT_GITHUB}")
 
   if [ "${_ref_type}" = "tag" ]; then
-    echo "${_ref_name}"
+    echo "${_ref_name}" | sed -E 's/^v//'
   fi
 }
 
@@ -54,16 +60,20 @@ fi
 
 fetch_depth="${1}"
 
-echo "Current directory: ${PWD}"
-echo "Git remotes: $(git remote -v)"
-echo "Current environment: "
-env | awk '{print "                     "$0}'
-echo "Current dir contents: "
-ls -A | awk '{print "                      "$0}'
+#echo "Current directory: ${PWD}"
+#echo "Git remotes: $(git remote -v)"
+#echo "Current environment: "
+#env | awk '{print "                     "$0}'
+#echo "Current dir contents: "
+#ls -A | awk '{print "                      "$0}'
+
+ghwa_debug "Fetch depth for version autodetection: ${fetch_depth}"
 
 if [ -z "${CONTEXT_GITHUB:+"not set"}" ]; then
   # We have JSON-encoded github context. Let's use it for version generation
   version_number="$(context_detect_version)"
+
+  ghwa_notice "Version, loaded from Action Context: ${version_number}"
 fi
 
 if [ -z "${version_number}" ]; then
@@ -71,6 +81,8 @@ if [ -z "${version_number}" ]; then
   # Generate version number from current git repository state
   git_fetch_history "${fetch_depth}"
   version_number="$(git_generate_version)"
+
+  ghwa_notice "Version, generated from git history: ${version_number}"
 fi
 
 ghwa_set_output "version" "${version_number}"
