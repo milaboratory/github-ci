3161bc43a25700f93b01a8c9096bf914411d1d8f
Korenevskiy Denis <denkoren@yandex.ru>
2021-11-18 15:52:25 +0300

    Attempt to fetch git history before calling 'git describe'

--- actions/autodetect-version/detect-version.sh
+++ actions/autodetect-version/detect-version.sh
@@ -10,6 +10,24 @@ version_number=""
 
 source "${script_path}/../lib/ghwa-commands.sh"
 
+# Fetches all references
+function git_fetch_history() {
+    local _depth="${1}"
+    local _current_sha
+
+    _current_sha="$(git rev-parse HEAD)"
+
+    # Fetch all git tags first. We don't need their history,
+    # but we have to know their SHAs.
+    # This command breaks the LOCAL history of origin, overwriting
+    # it with single commit for each remote ref (grafted commit)
+    git fetch --depth=1 --tags origin
+
+    # Fetch current commit's history up to ${_depth} records to restore
+    # HEAD's commits chain in hope we'll find at least one tag in there.
+    git fetch --depth="${_depth}" origin "${_current_sha}"
+}
+
 function git_generate_version() {
   git describe --tags |
     awk '{sub("-", ".", $0); print $0}' | # v1.0-2-g<hash> -> v1.0.2-g<hash>
@@ -29,7 +47,15 @@ function context_detect_version() {
   fi
 }
 
+if [ "${#}" -lt "1" ]; then
+  echo "Please, specify git history fetch depth" >&2
+  exit 64
+fi
+
+fetch_depth="${1}"
+
 echo "Current directory: ${PWD}"
+echo "Git remotes: $(git remote -v)"
 echo "Current environment: "
 env | awk '{print "                     "$0}'
 echo "Current dir contents: "
@@ -43,6 +69,7 @@ fi
 if [ -z "${version_number}" ]; then
   # We failed to determine version number from action context here.
   # Generate version number from current git repository state
+  git_fetch_history "${fetch_depth}"
   version_number="$(git_generate_version)"
 fi
 
