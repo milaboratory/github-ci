518aa02c423e8c998ff01ea1ef8ab7094852cbc4
Korenevskiy Denis <denkoren@yandex.ru>
2021-11-23 20:17:10 +0300

    Slightly change notice from autodetect-version action

--- actions/autodetect-version/src/main.ts
+++ actions/autodetect-version/src/main.ts
@@ -43,7 +43,7 @@ async function generateVersion(): Promise<void> {
   }
 
   if (version !== '') {
-    core.notice(`[autodetect-version]: detected version is ${version}`)
+    core.notice(`[autodetect-version]: version is ${version}`)
   }
   core.setOutput('version', version)
 }
