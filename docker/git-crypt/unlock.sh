114e46d2f9488e1789c1da20c69ad075d4b21be9
Korenevskiy Denis <denkoren@yandex.ru>
2022-03-29 21:16:29 +0300

    feat: add git-crypt docker

---
+++ docker/git-crypt/unlock.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+gpg_key_file="${HOME}/git-crypt-key.asc"
+
+function cleanup() {
+    rm "${gpg_key_file}"
+}
+
+function create-keyfile() {
+  echo "${GIT_CRYPT_PRIVATE_KEY}" | base64 -d > "${gpg_key_file}"
+
+  # schedule keyfile removal on script end by any reason
+  trap cleanup exit
+}
+
+function load-gpg-key() {
+  gpg --batch --import "${gpg_key_file}"
+  gpgconf --kill gpg-agent
+
+  gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
+  /usr/libexec/gpg-preset-passphrase \
+    --preset \
+    --passphrase "${GIT_CRYPT_KEY_PASSWORD}" \
+    "${GIT_CRYPT_KEY_GRIP}"
+}
+
+create-keyfile
+load-gpg-key
+git-crypt unlock
