a5315ccb74c3e42e9d23789a2651f2702d973694
Vladimir Antropov <vladimir.antropov@milaboratories.com>
2023-10-11 13:51:36 +0200

    add additional logging

--- actions/node/package-publish-status/src/index.ts
+++ actions/node/package-publish-status/src/index.ts
@@ -5,8 +5,19 @@ import * as path from 'path'
 // Get the package name and version from package.json located at the given path.
 function getPackageJSON(packagePath: string): {name: string; version: string} {
   const fullPath = path.join(packagePath, 'package.json')
+
+  if (!fs.existsSync(fullPath)) {
+    throw new Error(`package.json not found at path: ${fullPath}`)
+  }
+
   const rawData = fs.readFileSync(fullPath, 'utf-8')
   const packageJson = JSON.parse(rawData)
+
+  if (!packageJson.name || !packageJson.version) {
+    throw new Error(
+      'package.json must contain both name and version properties.'
+    )
+  }
 
   return {
     name: packageJson.name,
@@ -25,16 +36,23 @@ async function main(): Promise<void> {
       getPackageJSON(packagePath)
 
     // Check if package exists in the registry
-    const response = await fetch(
-      `${npmRegistryUrl}/${packageName}/v/${packageVersion}`
-    )
+    const fetchUrl = `${npmRegistryUrl}/${packageName}/v/${packageVersion}`
+    console.log(`Fetching: ${fetchUrl}`)
+
+    const response = await fetch(fetchUrl)
 
     if (response.ok) {
       core.setOutput('exists', '1')
+      console.log(`Package ${packageName}@${packageVersion} found in registry.`)
     } else if (response.status === 404) {
       core.setOutput('exists', '0')
+      console.log(
+        `Package ${packageName}@${packageVersion} not found in registry.`
+      )
     } else {
-      throw new Error(`Unexpected response: ${response.statusText}`)
+      throw new Error(
+        `Unexpected response: ${response.statusText}, status code: ${response.status}`
+      )
     }
   } catch (error) {
     if (error instanceof Error) {
