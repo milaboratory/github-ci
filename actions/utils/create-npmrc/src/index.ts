21a367b0dce75f2de4e8f17e5e48b671e842b528
Vladimir Antropov <vladimir.antropov@milaboratories.com>
2024-09-20 23:59:08 +0300

    Merge to v4 2024 09 20 (#65)
    feat: create-npmrc and list-files ts actions
    merge v4-beta to v4

---
+++ actions/utils/create-npmrc/src/index.ts
@@ -0,0 +1,39 @@
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function run(): Promise<void> {
+  try {
+    const configInput = core.getInput('npmrcConfig');
+    const config = JSON.parse(configInput);
+
+    const workspacePath = process.env.GITHUB_WORKSPACE;
+    if (!workspacePath) {
+      throw new Error('GITHUB_WORKSPACE not defined');
+    }
+
+    const npmrcPath = path.join(workspacePath, '.npmrc');
+    let npmrcContent = '';
+
+    Object.entries(config.registries).forEach(([registryUrl, scopes]) => {
+      Object.entries(scopes as Record<string, string>).forEach(([scope, tokenVar]) => {
+        npmrcContent += `@${scope}:registry=${registryUrl}\n`;
+        const registryURL = new URL(registryUrl);
+        npmrcContent += `//${registryURL.hostname}/${scope}/:_authToken=\${${tokenVar}}\n
+        `;
+      });
+    });
+
+    fs.writeFileSync(npmrcPath, npmrcContent);
+
+    core.setOutput('npmrcPath', npmrcPath);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('Unknown error occurred');
+    }
+  }
+}
+
+run();
