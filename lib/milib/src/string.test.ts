0b7794a1303342bebfbf595d796d171233cdf19a
Korenevskiy Denis <denkoren@gmail.com>
2025-06-09 19:44:53 +0300

    make this repo to be pnpm monorepo with milib in separate package

--- lib/milib/src/string.test.ts
+++ lib/milib/src/string.test.ts
@@ -1,5 +1,5 @@
-import { expect, test } from "@jest/globals";
-import { countOccurrences } from "../lib/string";
+import { expect, test } from "vitest";
+import { countOccurrences } from "./string";
 
 /*
  * countOccurrences tests
