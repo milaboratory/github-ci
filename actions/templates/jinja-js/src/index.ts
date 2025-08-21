ce26d15f959e8ac50e6920a2a607cca48d6f8c08
Vladimir Antropov <vladimir.antropov@milaboratories.com>
2025-08-21 14:57:05 +0000

    Merge to v4 2025 08 21 (#100)
    merge v4-beta to v4

---
+++ actions/templates/jinja-js/src/index.ts
@@ -0,0 +1,44 @@
+import * as core from '@actions/core';
+import * as nunjucks from 'nunjucks';
+
+// Export for testing
+export function run(): void {
+  try {
+    const dataInput = core.getInput('data', { required: true });
+    const template = core.getInput('template', { required: true });
+    const githubContext = core.getInput('githubContext');
+
+    const data = JSON.parse(dataInput);
+
+    if (githubContext) {
+      data.github = JSON.parse(githubContext);
+    }
+
+    const env = new nunjucks.Environment(null, {
+      autoescape: false,
+      throwOnUndefined: true,
+      trimBlocks: true,
+      lstripBlocks: true,
+    });
+    
+    // Add a custom filter to convert objects to JSON strings
+    env.addFilter('json', function (value) {
+      return JSON.stringify(value, null, 2);
+    });
+
+    const result = env.renderString(template, data);
+
+    core.setOutput('result', result);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('An unknown error occurred.');
+    }
+  }
+}
+
+// Run the action if not in a test environment
+if (process.env.JEST_WORKER_ID === undefined) {
+  run();
+}
