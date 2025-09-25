ec5a6421722662d004404aa12fc3311d87ad5b08
Korenevskiy Denis <denkoren@gmail.com>
2025-09-25 21:44:16 +0300

    fix merge script

--- merge-beta.sh
+++ merge-beta.sh
@@ -28,26 +28,39 @@ if [ "${__REAL_RUN:-}" != "true" ]; then
     exit 0
 fi
 
+echo "Updating remote repository info..."
 git fetch --prune origin
 if git branch | grep -qE " ${MERGE_BRANCH}( |$)"; then
+    echo "Found local merge branch"
+    echo "  switching to '${MERGE_BRANCH}'..."
     # Merge branch exists in local repository
     git checkout "${MERGE_BRANCH}"
+    echo "  merging '${TARGET_BRANCH}'..."
     git merge --no-edit "${TARGET_BRANCH}"
 
-else if git branch --remotes | grep -qE " origin/${MERGE_BRANCH}( |$)"; then
-    # Merge branch exists in remote repository
+elif git branch --remotes | grep -qE " origin/${MERGE_BRANCH}( |$)"; then
+    echo "Found merge branch in remote repository"
+    echo "  switching to 'origin/${MERGE_BRANCH}'..."
     git checkout "origin/${MERGE_BRANCH}"
+    echo "  creating local '${MERGE_BRANCH}' branch..."
     git checkout -b "${MERGE_BRANCH}"
+    echo "  merging '${TARGET_BRANCH}'..."
     git merge --no-edit "${TARGET_BRANCH}"
 
 else
+    echo "No existing merge branch found"
+    echo "  switching to '${TARGET_BRANCH}'..."
+    git checkout "${TARGET_BRANCH}"
+    git pull || true
+    echo "  creating '${MERGE_BRANCH}' branch..."
     git checkout -b "${MERGE_BRANCH}"
 fi
 
 if git branch | grep -qE " ${SOURCE_BRANCH}( |$)"; then
-    # Source branch exists in local repository
-    true
+    echo "Found source branch in local repository, syncing it with remote..."
+    git fetch origin "${SOURCE_BRANCH}:${SOURCE_BRANCH}" || true
 else
+    echo "No source branch found in local repository, using remote..."
     SOURCE_BRANCH="origin/${SOURCE_BRANCH}"
 fi
 
@@ -61,14 +74,17 @@ git merge \
     find . -type f -name "action.yaml"
     find .github/workflows -type f -name "*.yaml"
 } |
-    xargs -L 1 \
-    sed -i "s/${SOURCE_BRANCH}/${TARGET_BRANCH}/g"
+    while read -r file; do
+        sed "s/@${SOURCE_BRANCH}/@${TARGET_BRANCH}/g" "${file}" > "${file}.tmp"
+        mv "${file}.tmp" "${file}"
+    done
 
 git add .
 git commit --amend --no-edit
 git push origin "${MERGE_BRANCH}"
 
 gh pr create \
-    --title "Merge ${TARGET_BRANCH}-beta into ${TARGET_BRANCH}" \
+    --title "Merge ${SOURCE_BRANCH} to ${TARGET_BRANCH} (${DATE})" \
+    --body "Merge ${SOURCE_BRANCH} into ${TARGET_BRANCH}" \
     --base "${TARGET_BRANCH}" \
     --head "${MERGE_BRANCH}"
