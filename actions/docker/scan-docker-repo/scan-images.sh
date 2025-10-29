b3ea39367695c5b2caf0032703449a79fda82699
Korenevskiy Denis <denkoren@gmail.com>
2025-10-29 21:44:05 +0300

    action stabilisation

--- actions/docker/scan-docker-repo/scan-images.sh
+++ actions/docker/scan-docker-repo/scan-images.sh
@@ -9,9 +9,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #
 # Positional parameters
 #
-registry="${1}" # i.e. containers.pl-open.science
-repository="${2}" # i.e. milaboratories/pl-containers
-shift 2
 tags=("${@}") # scan given tags only (no listing)
 
 #
@@ -23,9 +20,7 @@ tags=("${@}") # scan given tags only (no listing)
 #
 # Special interface for action
 #
-: "${TAGS:=}" # scan given tags only (for simpler action code)
 : "${TAG_FILE:=}" # file with list of tags to scan (parallel scanning feature)
-: "${SKIPPED_LIST_FILE:=$(mktemp)}" # file with list of actually skipped images
 
 #
 # Trivy control
@@ -48,15 +43,10 @@ log() {
     logf "%s\n" "$*"
 }
 
-read_tags_list() {
-    local _t
-    while read -r t; do
-        tags+=("${t}")
-    done < <(grep --extended-regexp --invert-match '^#|^ *$')
-}
-
+scan_count=0
 scan_image() {
     local _image="$1"
+    local _limit="${2:-}"
 
     local _opts=(
         --format "${REPORT_FORMAT}"
@@ -79,6 +69,12 @@ scan_image() {
         return 0
     fi
 
+    if [ -n "${_limit}" ] && [ "${scan_count}" -ge "${_limit}" ]; then
+        log "  reached scan limit of ${_limit} images"
+        return 0
+    fi
+
+    scan_count=$((scan_count + 1))
     log "  scanning ${_image}"
 
     if [ -n "${REPORT_FILE}" ]; then
@@ -94,63 +90,33 @@ scan_image() {
     fi
 }
 
-scan_images() {
-    local _limit="${1:-}"
-    local _success=true
-
-    local _items_count=0
-    while read -r tag; do
-        scan_image "${tag}" || _success=false
-
-        _items_count=$((_items_count + 1))
-        if [ -n "${_limit}" ] && [ "${_items_count}" -ge "${_limit}" ]; then
-            log "  reached scan limit of ${_limit} images"
-            break
-        fi
-    done
-
-    if [ "${_success}" != "true" ]; then
-        return 1
-    fi
-}
-
 #
 # Script body
 #
-
-if [ -n "${SKIPPED_LIST_FILE}" ] && [ ! -f "${SKIPPED_LIST_FILE}" ]; then
-    log "Skipped list file: ${SKIPPED_LIST_FILE}"
-    printf "" > "${SKIPPED_LIST_FILE}"
-fi
-
 if [ -n "${REPORT_FILE}" ]; then
     log "Report file: ${REPORT_FILE}"
     printf "" > "${REPORT_FILE}"
 fi
 
-# Read tags list from action inputs
-if [ -n "${TAGS}" ]; then
-    read_tags_list <<< "${TAGS}"
-fi
-
-# Read tags list from file
 if [ -n "${TAG_FILE}" ]; then
-    read_tags_list < <(cat "${TAG_FILE}")
+    while read -r _tag; do
+        tags+=("${_tag}")
+    done < <(
+        grep \
+            --extended-regexp \
+            --invert-match \
+            '^#|^ *$' \
+            "${TAG_FILE}"
+    )
 fi
 
 cmd_example=$(DRY_RUN="y" scan_image "<image-tag>")
 logf "Analysis command:\n  ${cmd_example}\n\n"
 
 success=true
-if [ -n "${tags:-}" ]; then
-    for tag in "${tags[@]}"; do
-        scan_image "${registry}/${repository}:${tag}" || success=false
-    done
-else
-    log "Scanning images in ${registry}/${repository}..."
-    "${script_dir}/list-images.sh" "${registry}" "${repository}" "${SCAN_IMAGES_LIMIT}" |
-        scan_images "${SCAN_IMAGES_LIMIT}" || success=false
-fi
+for tag in "${tags[@]}"; do
+    scan_image "${tag}" "${SCAN_IMAGES_LIMIT}" || success=false
+done
 
 if [ "${success}" == "true" ]; then
     log "Scan completed successfully, no CVEs found!"
@@ -207,8 +173,5 @@ if [ -n "${REPORT_FILE}" ] && [ "${REPORT_FORMAT}" == "json" ]; then
             ] |
                 .[0] + " (" + .[1] + ")"' >&2
 fi
-
-logf "Skipped images: %d\n" "$(wc -l "${SKIPPED_LIST_FILE}" | awk '{print $1}')"
-[ "${DEBUG}" == "true" ] && cat "${SKIPPED_LIST_FILE}" | awk '{printf "  %s\n", $0}' >&2
 
 exit 1
